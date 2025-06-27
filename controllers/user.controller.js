89274c77b95e862cb6b06b3a57f9043ad3072bac
dhiraj <dhirajmymodak22@gmail.com>
2025-06-27 12:32:59 +0530

    axios added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,6 +3,7 @@ const Cloudinary = require("cloudinary");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../config/config");
 const bcrypt = require("bcryptjs");
+const axios = require("axios");
 
 const checkUserExists = async (req, res) => {
   try {
