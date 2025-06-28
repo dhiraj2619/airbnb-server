3862abb9e84a9e20d6087668e77829217d560eed
dhiraj <dhirajmymodak22@gmail.com>
2025-06-28 12:45:49 +0530

    push

--- models/user.model.js
+++ models/user.model.js
@@ -1,67 +1,74 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({ 
-    firstName: {
-        type: String,
-        required: [true, "Please enter your first name"],
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please enter your first name"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please enter your last name"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your email address"],
+    unique: true,
+    match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+  },
+  mobile: {
+    type: String,
+    required: function () {
+      return !this.googleId;
     },
-    lastName: {
-        type: String,
-        required: [true, "Please enter your last name"],
+    match: [/^\d{10}$/, "Please enter a valid 10-digit mobile number"],
+  },
+  password: {
+    type: String,
+    required: function () {
+      return !this.googleId;
     },
-    email: {
-        type: String,
-        required: [true, "Please enter your email address"],
-        unique: true,
-        match: [/^\S+@\S+\.\S+$/, "Please enter a valid email address"],
+    minlength: [6, "Password must be at least 6 characters long"],
+  },
+  dateofbirth: {
+    type: Date,
+    required: function () {
+      return !this.googleId;
     },
-    mobile: {
-        type: String,
-        required: [true, "Please enter your mobile number"],
-        match: [/^\d{10}$/, "Please enter a valid 10-digit mobile number"],
+  },
+  profilePic: {
+    public_id: {
+      type: String,
+      required: [false, "Please upload a profile picture"],
     },
-    password: {
-        type: String,
-        required: [true, "Please enter your password"],
-        minlength: [6, "Password must be at least 6 characters long"],
+    url: {
+      type: String,
+      required: [false, "Please upload a profile picture"],
     },
-    dateofbirth:{
-        type:Date,
-         required:[true,"Please enter your date of birth"],
+  },
+  role: {
+    type: String,
+    enum: ["user", "host", "admin"],
+    default: "user",
+  },
+  googleId: {
+    type: String,
+    default: null,
+  },
+  address: {
+    type: String,
+    required: function () {
+      return !this.googleId;
     },
-    profilePic:{
-        public_id: {
-            type: String,
-            required: [false, "Please upload a profile picture"],
-        },
-        url: {
-            type: String,
-            required: [false, "Please upload a profile picture"],
-        },  
-    },
-    role: {
-        type: String,
-        enum: ["user", "host", "admin"],
-        default: "user",
-    },
-    googleId: {
-        type: String,
-        default: null,
-    },
-    address: {
-        type: String,
-        required: [false, "Please enter your address"],
-    },
-    
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    
- });
+  },
 
- userSchema.virtual("isGoogleUser").get(function(){
-    return  !!this.googleId;
- })
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
- module.exports = mongoose.model("User", userSchema);
+userSchema.virtual("isGoogleUser").get(function () {
+  return !!this.googleId;
+});
+
+module.exports = mongoose.model("User", userSchema);
