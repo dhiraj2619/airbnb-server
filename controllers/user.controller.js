bd679fd4dda60e01e0c0bac87fad7fcc25e6031e
dhiraj <dhirajmymodak22@gmail.com>
2025-06-30 22:37:52 +0530

    logout user controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -214,6 +214,15 @@ const CompleteProfile = async (req, res) => {
   }
 };
 
+const logoutUser = async (req, res) => {
+  try {
+    
+    return res.status(200).json({ success: true, message: "Logged out successfully" });
+  } catch (error) {
+    console.error("Logout error:", error);
+    return res.status(500).json({ success: false, message: "Server error during logout" });
+  }
+};
 module.exports = {
   signupUser,
   loginUser,
